35bbf916dd5735653fcf6d16e4a5e28329e75201
Junseok Oh <kotohana5706@edcan.kr>
2015-11-06 03:14:13 +0900

    working : add node modules

--- routes/auth.js
+++ routes/auth.js
@@ -10,12 +10,12 @@ module.exports = router;
 
 router.post('/login', function (req, res) {
   var user_id = req.body.id;
-  var token = req.body.token;
+  // var token = req.body.token;
   var user_pwd = req.body.password;
   mongo.User.findOne({id: user_id}, function(err, doc){
     if(doc!=null){
       if(doc.id == user_id && doc.password == user_pwd){
-        mongo.User.update({id : user_id}, {isLogin : true, token : token}, function(err, numAffected){
+        mongo.User.update({id : user_id}, {isLogin : true}, function(err, numAffected){
           if(err) throw err;
         });
         res.send(doc);
@@ -37,11 +37,11 @@ router.post('/logout', function(req, res){
 
 router.post('/loginValidate', function(req, res){
   var user_apikey = req.body.apikey;
-  var token = req.body.token;
+  // var token = req.body.token;
   mongo.User.findOne({apikey : user_apikey}, function(err, doc){
     if(doc!=null){
       if (doc.isLogin == true) {
-        mongo.User.update({apikey : user_apikey}, {token : token}, function(err, numAffected){
+        mongo.User.update({apikey : user_apikey}, function(err, numAffected){
             if(err) res.sendStatus(400);
             else res.sendStatus(200);
         })
@@ -55,7 +55,6 @@ router.post('/register', function (req, res) {
   var user_id = req.body.id;
   var user_pwd = req.body.password;
   var user_name = req.body.name;
-  var type = req.body.isParent;
   mongo.User.find({id : user_id}, function(err, docs){
     if(docs.length==0) {
       var newUser = new mongo.User({
@@ -63,12 +62,7 @@ router.post('/register', function (req, res) {
         password : user_pwd,
         name : user_name,
         apikey : randomstring.generate(),
-        isParent : type,
-        isLogin : false,
-        sticker : 0,
-        targetApikey : undefined,
-        targetName : undefined,
-        token : undefined
+        isLogin : false
       });
       newUser.save();
       res.send(newUser);
